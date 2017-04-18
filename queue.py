7819f282145e18333432aeaa1d747f40848bcb7b
Jason Liang <jason.liang@jliang-mbp.geneticfinance.com>
2017-04-17 21:57:38 -0500

    added queue

---
+++ queue.py
@@ -0,0 +1,58 @@
+import os, sys, time, random, glob, md5
+import jsonpickle
+
+"""A collection of persistent file system based queues that can be used
+for completion service"""
+
+class TaskObject(object):
+  """Wrapper class for a task"""
+  def __init__(task=None, task_data=None, is_result=False):
+    if is_result:
+      assert task is None
+    self.time_created = time.time()
+    self.num_failures = 0
+    self.is_result = is_result
+    self.task = task
+    self.task_data = task_data
+
+class FIFOQueue(object):
+  """First in first out based on timestamps"""
+  def __init__(self, queue_dir):
+    self.queue_dir = queue_dir
+    if not os.path.exists(self.queue_dir):
+      print "creating new FIFO queue_dir: %s", % self.queue_dir
+      os.makedirs(self.queue_dir)
+    else:
+      print "using existing queue_dir for FIFO: %s", self.queue_dir
+
+  def push(self, task_object):
+    task_obj_str = jsonpickle.encode(task_object)
+    task_obj_hash = md5.new(task_obj_str).digest()
+    task_file = "%s_%s.task" % (time.time(), task_obj_hash)
+    with open(os.path.join(self.queue_dir, task_file), 'wb') as f:
+      f.write(task_obj_str)
+
+  def __find_earliest_task(self):
+    earliest_timestamp = 1e308
+    earliest_task = None
+    for task_file in glob.glob(os.path.join(self.queue_dir, "*.task")):
+      task_file_name = task_file.split("/")[-1]
+      task_timestamp = float(task_file_name.split("_")[0])
+      if task_timestamp < earliest_timestamp:
+        earliest_timestamp = task_timestamp
+        earliest_task = task_file
+    return earliest_task
+
+  def pop(self, new_name=None):
+    earliest_task = self.__find_earliest_task()
+
+    if earliest_task is None:
+      return None
+
+    with open(earliest_task, 'rb') as f:
+      task_object = jsonpickle.decode(f.read())
+    if new_name is None:
+      os.remove(earliest_task)
+    else:
+      os.rename(earliest_task, new_name)
+    return task_object
--- server.py
+++ server.py
@@ -1,10 +1,20 @@
 import os, sys, time, random, ast
 import pickle as cPickle
 
+from queue import FIFOQueue, TaskObject
+
+"""Default config file settings"""
 DEFAULT_CONFIG = \
-  {
-    "n_clients": 1,
-  }
+{
+  # Number of client machines to spawn
+  "n_clients": 1,
+  # Base directory where all the files are located
+  "base_dir:": "/tmp/completion_service",
+  # Modes, allowed are: local and condor
+  "mode": "local",
+  # Maximum size in kB for task data
+  "task_data_maxsize": 64
+}
 
 class CompletionServiceServer(object):
   """Class for server that sends out tasks to clients"""
@@ -12,7 +22,21 @@ class CompletionServiceServer(object):
     self.config = DEFAULT_CONFIG
     if config_file is not None:
       with open(config_file) as f:
-        self.config = ast.literal_eval(f.read())
+        self.config.update(ast.literal_eval(f.read()))
+
+    if not os.path.exists(self.config['base_dir']):
+      os.makedirs(self.config['base_dir'])
+
+    self.queue = Queue()
+
+  def submitTask(self, task, task_data):
+    if len(taskData)/1000 > self.config['task_data_maxsize']:
+      print "task data size (%s kb) exceeded maximum size (%s kb)" % \
+        (len(task_data)/1000, self.config['task_data_maxsize'])
+      return
+
+    task_object = TaskObject(task, task_data, is_result=False)
+
 
 if __name__ == "__main__":
   pass
